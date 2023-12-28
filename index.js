a5c9983ff5458dcfe4dc242f33a7a9731710611c
Sh44n <shaanedroos85@gmail.com>
2023-12-28 17:13:55 +0530

    added ejs-mate tool for boilerplate purposes

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 const mongoose = require("mongoose");
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
@@ -18,6 +19,7 @@ const Campground = require("./models/campground")
 
 app.set("view engine", "ejs")
 app.set("views", path.join(__dirname, "/views"))
+app.engine("ejs", ejsMate)
 
 app.use(express.urlencoded({extended:true}))
 app.use(methodOverride("_method"))
