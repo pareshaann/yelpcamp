3e47f7dc9e915672001a7cb2efd625b37c936a93
Sh44n <shaanedroos85@gmail.com>
2023-12-30 18:15:08 +0530

    moved JOI schema to another file and tidied a bit of code

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const {campgroundSchema} = require("./schemas") //destructured so we can add more schemas later on
 const ExpressError = require("./utils/ExpressError")   // import custom Error class
 const wrapAsync = require("./utils/wrapAsync")     // import async error handler
 
@@ -26,6 +27,18 @@ app.engine("ejs", ejsMate)
 app.use(express.urlencoded({extended:true}))                    //req.body parser
 app.use(methodOverride("_method"))                              //used to send put and patch requests from html forms
 
+const validateCampground = (req, res, next) => {                    //campground validator middleware using JOI
+    
+    const {error} = campgroundSchema.validate(req.body);
+    if(error){
+        const msg = error.details.map(el => el.message).join(", ")   
+        //error.details is an array on which .map() function is used. This returns an array of only the error messages. On this array, the .join(", ") function is used, which creates and returns a string of all elements seperated by a comma.
+        throw new ExpressError(msg, 400)                          
+    } else{
+        next();
+    }    
+}
+
 app.get("/", (req, res)=>{
     res.render("home")
 });
@@ -36,10 +49,10 @@ app.get("/campgrounds", wrapAsync(async(req, res) => {
 app.get("/campgrounds/new",(req, res) => {
     res.render("campgrounds/new")
 })
-app.post("/campgrounds", wrapAsync(async(req,res) => {
-    if(!req.body.campground){               //Error thrown in absence of required model  properties like name, location etc 
-        throw new ExpressError("Incomplete data to create Campground!", 400)
-    }
+app.post("/campgrounds", validateCampground, wrapAsync(async(req,res) => {
+    // if(!req.body.campground){               //Error thrown in absence of required model  properties like name, location etc 
+    //     throw new ExpressError("Incomplete data to create Campground!", 400)
+    // }                                                    
     const newCampground = new Campground(req.body.campground);
     await newCampground.save();
     res.redirect(`/campgrounds/${newCampground._id}`)
@@ -54,7 +67,7 @@ app.get("/campgrounds/:id/edit", wrapAsync(async(req,res) => {
     const camp = await Campground.findById(id);
     res.render("campgrounds/edit", { camp })
 }));
-app.put("/campgrounds/:id", wrapAsync(async(req, res) => {
+app.put("/campgrounds/:id", validateCampground, wrapAsync(async(req, res) => {
     const { id } = req.params;
     const updatedCamp = await Campground.findByIdAndUpdate(id, req.body.campground, {new: true});
     res.redirect(`/campgrounds/${updatedCamp._id}`);
@@ -71,7 +84,9 @@ app.all("*", (req, res, next) => {
 
 app.use((err, req, res, next) => {              // ERROR HANDLER
     const {status = 500} = err                
-    if(!err.message) err.message = "Oh no! Something went wrong :("
+    if(!err.message) {
+        err.message = "Oh no! Something went wrong :("
+    }
     res.status(status).render("error", {err})
 })
 
